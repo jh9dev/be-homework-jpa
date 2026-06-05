82ae6095f09c13ed2b99034d87b711fd0e963cec
jh9dev <sungjuhyun1001@gmail.com>
2026-06-05 19:21:12 +0900

    feat: Order와 Coffee 연관 관계 매핑

---
+++ src/main/java/com/springboot/order/entity/OrderCoffee.java
@@ -0,0 +1,35 @@
+package com.springboot.order.entity;
+
+import com.springboot.coffee.entity.Coffee;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+public class OrderCoffee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long orderCoffeeId;
+
+    @ManyToOne
+    @JoinColumn(name = "ORDER_ID")
+    private Order order;
+
+    @ManyToOne
+    @JoinColumn(name = "COFFEE_ID")
+    private Coffee coffee;
+
+    @Column(nullable = false)
+    private int quantity;
+}
